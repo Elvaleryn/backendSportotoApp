fd6c6a9f040f7cf3d0d7c8fac955514cb8fbd3c2
elvaleryn <eraykaya121@gmail.com>
2020-02-17 22:03:33 +0300

    build added

---
+++ build/precache-manifest.da9935cfa76ff535718f0c3e18b4a5d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f1425339b3537b28ec9264cba2a5371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baea77b46919276d97bd",
+    "url": "/static/js/2.fcd4449e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fcd4449e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ee9f80dc851e0bbb52",
+    "url": "/static/js/main.8b4087f7.chunk.js"
+  },
+  {
+    "revision": "846f75bde6c302397c9c",
+    "url": "/static/js/runtime-main.57f600ab.js"
+  }
+]);
